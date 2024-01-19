a48f462e8464f99942e7389bedec2f292615bccc
alvinacchow <alvinacchow@gmail.com>
2024-01-19 14:23:12 -0800

    grid

--- sportsApp/Views/MainScreen.swift
+++ sportsApp/Views/MainScreen.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct MainScreen: View {
+    private var data: [Int] = Array (1...6)
+    private let colors: [Color] = [.red, .green, .blue, .yellow]
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 400))
+    ]
     
     var body: some View {
         NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+           
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 40) {
+                    ForEach(data, id:\.self) { number in
+                        ZStack {
+                            Rectangle()
+                                .frame(width:400, height:400)
+                                .foregroundColor(colors[number%4])
+                                .cornerRadius(30)
+                            Text("\(number)")
+                                .foregroundColor(.white)
+                                .font(.system(size:80, weight: .medium, design: .rounded))
+                        }
+                    }
+                }
+            }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
+        
     }
-    
+        
 }
 
 #Preview {
