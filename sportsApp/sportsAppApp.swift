cb6a9a0912bff6af6213121e243891ab1b5dca62
alvinacchow <alvinacchow@gmail.com>
2024-01-18 16:41:07 -0800

    Initial Commit

---
+++ sportsApp/sportsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportsAppApp.swift
+//  sportsApp
+//
+//  Created by Alvina Chow on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct sportsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
