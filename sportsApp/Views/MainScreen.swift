03e95817b8d3bee60d1888fba09a90817c823c91
alvinacchow <alvinacchow@gmail.com>
2024-01-19 22:14:01 -0800

    background fill

--- sportsApp/Views/MainScreen.swift
+++ sportsApp/Views/MainScreen.swift
@@ -36,31 +36,60 @@ struct MainScreen: View {
         GridItem(.adaptive(minimum: 300))
     ]
     
+    
+    func setGradientBackground() -> some View {
+        let colorTop = Color(
+            red: Double(0xfe) / 255.0,
+            green: Double(0xd9) / 255.0,
+            blue: Double(0xa1) / 255.0
+        )
+        
+        let colorBottom = Color(
+            red: Double(0xf7) / 255.0,
+            green: Double(0xa1) / 255.0,
+            blue: Double(0x5e) / 255.0
+        )
+        
+        return LinearGradient(gradient: Gradient(colors: [colorTop, colorBottom]), startPoint: .top, endPoint: .bottom)
+    }
+    
+    
     var body: some View {
+        
         NavigationView {
             ScrollView {
+                
                 LazyVGrid(columns: adaptiveColumns, spacing: 30) {
                     ForEach(athletes) { athlete in
                         ZStack {
-                            athlete.image
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 300, height: 300)
-                                .cornerRadius(30)
-                            
-                            Text(athlete.title)
-                                .foregroundColor(.white)
-                                .font(.system(size: 20, weight: .medium, design: .rounded))
+                            VStack {
+                                athlete.image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 300, height: 300)
+                                    .cornerRadius(300)
+                                
+                                Text(athlete.title)
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 20, weight: .medium, design: .rounded))
+                            }
                         }
                     }
                 }
             }
+            .padding()
+            .background(setGradientBackground())
+            .ignoresSafeArea()
+            
+
             
         }
         .padding()
         .navigationViewStyle(StackNavigationViewStyle())
-        
+        .background(setGradientBackground())
+        .ignoresSafeArea()
     }
+    
         
 }
 
