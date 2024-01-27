084694d568eb3b32e45ac5356a06c943ab8c1806
alvinacchow <alvinacchow@gmail.com>
2024-01-27 14:11:53 -0800

    home page

--- sportsApp/Views/HomePage.swift
+++ sportsApp/Views/HomePage.swift
@@ -23,85 +23,89 @@ struct HomePage: View {
                 ZStack {
                     Image("Banner")
                         .resizable()
+                        .opacity(0.6)
                         .scaledToFill()
                         .frame(width: UIScreen.main.bounds.width, height: 500)
                         .clipped()
+                
                     
                     
                     Text("HER Game, HER Rules, TrailblazHER")
                         .foregroundColor(.white)
-                        .font(Font.custom("Nexa-Trial-BoldItalic", size: 50))
+                        .font(Font.custom("Nexa-Trial-HeavyItalic", size: 60))
                         .frame(maxWidth: .infinity, alignment: .center)
                     
                 }
-                .offset(y:-50)
 
-                
-                ZStack{
-                    LazyVStack(alignment: .center, spacing: 30) {
+                HStack {
+                    NavigationLink (destination: AthleteScreen()) {
                         ZStack {
-                            VStack(alignment: .center) {
+                            Rectangle()
+                                .frame(width: 375, height: 450)
+                                .foregroundColor(Color(hex: "#FFC4A3"))
+                                .cornerRadius(30)
+                        
+                            VStack {
+                                Image("Runner")
+                                    .resizable()
+                                    .scaledToFit()
                                 
-                                NavigationLink (destination: AthleteScreen()) {
-                                    ZStack {
-                                        Rectangle()
-                                            .frame(width: 600, height: 100)
-                                            .foregroundColor(Color(hex: "#FFC4A3"))
-                                            .cornerRadius(30)
-                                        
-                                        Text("Athletes")
-                                            .foregroundColor(.white)
-                                            .font(Font.custom("Nexa-Trial-RegularItalic", size: 30))
-                                    }
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30)
-                                            .stroke(.white, lineWidth: 3))
-                                    
-                                }
-                                .padding(10)
-                                
-                                NavigationLink(destination: SportScreen()) {
-                                    ZStack {
-                                        Rectangle()
-                                            .frame(width: 600, height: 100)
-                                            .cornerRadius(30)
-                                            .foregroundColor(Color(hex: "#FF9A76"))
-                                        
-                                        Text("Sports")
-                                            .foregroundColor(.white)
-                                            .font(Font.custom("Nexa-Trial-RegularItalic", size: 30))
-                                    }
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30)
-                                            .stroke(.white, lineWidth: 3))
-                                    
-                                }
-                                .padding(10)
-                                
-                                NavigationLink(destination: GearScreen()) {
-                                    ZStack {
-                                        Rectangle()
-                                            .frame(width: 600, height: 100)
-                                            .cornerRadius(30)
-                                        
-                                            .foregroundColor(Color(hex: "#F96D80"))
-                                        Text("Gear")
-                                            .foregroundColor(.white)
-                                            .font(Font.custom("Nexa-Trial-RegularItalic", size: 30))
-                                    }
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30)
-                                            .stroke(.white, lineWidth: 3))
-                                }
-                                .padding(10)
+                                Text("Athletes")
+                                    .foregroundColor(.white)
+                                    .font(Font.custom("Nexa-Trial-RegularItalic", size: 30))
+                                    .offset(y: -10)
                             }
                         }
                     }
+                    .padding(20)
+                    
+                    
+                    NavigationLink(destination: SportScreen()) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 375, height: 450)
+                                .cornerRadius(30)
+                                .foregroundColor(Color(hex: "#FF9A76"))
+                            
+                            VStack {
+                                Image("Gloves")
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                
+                                Text("Sports")
+                                    .foregroundColor(.white)
+                                    .font(Font.custom("Nexa-Trial-RegularItalic", size: 30))
+                                    .offset(y: -10)
+                            }
+                        }
+                        
+                    }
+                    .padding(20)
+                    
+                    NavigationLink(destination: GearScreen()) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 375, height: 450)
+                                .cornerRadius(30)
+                                .foregroundColor(Color(hex: "#F96D80"))
+                            VStack {
+                                Image("Shoes")
+                                    .resizable()
+                                    .scaledToFit()
+                        
+                                Text("Gear")
+                                    .foregroundColor(.white)
+                                    .font(Font.custom("Nexa-Trial-RegularItalic", size: 30))
+                                    .offset(y: -10)
+                            }
+                        }
+                    }
+                    .padding(20)
                 }
             }
-            
+            .padding(.bottom, 100)
         }
-        
         
     .navigationViewStyle(StackNavigationViewStyle())
     .navigationBarHidden(true)
